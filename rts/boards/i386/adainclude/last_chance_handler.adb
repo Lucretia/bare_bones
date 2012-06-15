a2dea2e35885685759858bfdcc9f4cd233189c23
Luke A. Guest <laguest@archeia.com>
2012-06-15 12:45:57 +0100

    Added a message to the bottom line of the console stating an exception had been raised.

--- rts/boards/i386/adainclude/last_chance_handler.adb
+++ rts/boards/i386/adainclude/last_chance_handler.adb
@@ -4,12 +4,18 @@
 --  Author          : Luke A. Guest
 --  Created On      : Thu Jun 14 12:06:48 2012
 --  Licence         : See LICENCE in the root directory.
+with Console; use Console;
+
 procedure Last_Chance_Handler
   (Source_Location : System.Address; Line : Integer) is
    pragma Unreferenced (Source_Location, Line);
 begin
    --  TODO: Add in code to dump the info to serial/screen which
    --  is obviously board specific.
+   Put ("Exception raised",
+        Screen_Width_Range'First,
+        Screen_Height_Range'Last);
+
    loop
       null;
    end loop;
