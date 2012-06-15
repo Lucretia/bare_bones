246b9f5a44be3c526fc8cc64e147906d774f1c5e
Luke A. Guest <laguest@archeia.com>
2012-06-15 20:17:25 +0100

    Added some more comments.

--- src/pc/startup.s
+++ src/pc/startup.s
@@ -6,7 +6,8 @@
 .set FLAGS,    ALIGN | MEMINFO          # this is the Multiboot 'flag' field
 .set MAGIC,    0x1BADB002               # 'magic number' lets bootloader find the header
 .set CHECKSUM, -(MAGIC + FLAGS)         # checksum required
- 
+
+header:	
 .align 4
 .long MAGIC
 .long FLAGS
@@ -15,16 +16,23 @@
 # reserve initial kernel stack space
 .set STACKSIZE, 0x4000                  # that is, 16k.
 .lcomm stack, STACKSIZE                 # reserve 16k stack on a doubleword boundary
-.comm  mbd, 4                           # we will use this in kmain
-.comm  magic, 4                         # we will use this in kmain
+.comm  mbd, 4                           # we will use this in Bare_Bones
+.comm  magic, 4                         # we will use this in Bare_Bones
  
 startup:
     movl  $(stack + STACKSIZE), %esp    # set up the stack
+
+# The following saves the contents of the registers as they will likely be
+# overwritten as main is not our actual entry point, Bare_Bones is. We will
+# use these 2 variables inside Bare_Bones.
+	
     movl  %eax, magic                   # Multiboot magic number
     movl  %ebx, mbd                     # Multiboot data structure
  
     call  main                          # call main created by gnatbind
- 
+
+# In a real kernel, execution should never get to this point.
+	
     cli
 hang:
     hlt                                 # halt machine should kernel return
