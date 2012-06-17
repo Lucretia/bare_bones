e91ebec06bf858ee75cac5ccd264f2a9369d502c
Luke A. Guest <laguest@archeia.com>
2012-06-17 15:24:54 +0100

    Make this package Preelaborate so a-except can depend on it.

--- rts/boards/i386/adainclude/last_chance_handler.ads
+++ rts/boards/i386/adainclude/last_chance_handler.ads
@@ -9,3 +9,4 @@ with System;
 procedure Last_Chance_Handler
   (Source_Location : System.Address; Line : Integer);
 pragma Export (C, Last_Chance_Handler, "__gnat_last_chance_handler");
+pragma Preelaborate (Last_Chance_Handler);
