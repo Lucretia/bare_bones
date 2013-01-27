e303b8ce4845010ccf2bbcd3323e2decd58e478a
David Rees <dave@eutopialabs.com>
2013-01-27 14:53:50 +0700

    Expand comments in startup.s, and pad header location counter with an explicit fill value

--- src/pc/startup.s
+++ src/pc/startup.s
@@ -1,39 +1,43 @@
-.global startup                         # Make entry point visible to linker
+.global startup                         # Make the startup entry point symbol visible to the linker
 
-                                        # Set up Multiboot header (see GRUB docs for details)
+                                        # Setup the Multiboot header (see GRUB docs for details)
 .set ALIGN,    1<<0                     # Align loaded modules on page boundaries
 .set MEMINFO,  1<<1                     # Provide memory map
 .set FLAGS,    ALIGN | MEMINFO          # This is the Multiboot 'flag' field
-.set MAGIC,    0x1BADB002               # 'magic number' lets bootloader find header
+.set MAGIC,    0x1BADB002               # 'magic number' lets your bootloader find the header
 .set CHECKSUM, -(MAGIC + FLAGS)         # Checksum required
 
-header:
-.align 4
+header:                                 # Must be in the first 8kb of the kernel file
+.align 4, 0x90                          # Pad the location counter (in the current subsection) to a 4-byte (DWORD) storage boundary.
+                                        # The way alignment is specified can vary with host system architecture.
 .long MAGIC
 .long FLAGS
 .long CHECKSUM
 
                                         # Reserve initial kernel stack space
 .set STACKSIZE, 0x4000                  # 0x4000 being 16k.
-.lcomm stack, STACKSIZE                 # Reserve 16k stack on a doubleword boundary
-.comm  mbd, 4                           # We will use this in Bare_Bones
-.comm  magic, 4                         # We will use this in Bare_Bones
+.lcomm stack, STACKSIZE                 # Reserve 16k stack on a doubleword (32bit) boundary
+.comm  mbd, 4                           # Declare common symbol mbd, allocate it 4-bytes (DWORD) of uninitialized memory.
+.comm  magic, 4                         # Declare common symbol magic, allocate it 4-bytes (DWORD) of uninitialized memory.
 
 startup:
     movl  $(stack + STACKSIZE), %esp    # Set up the stack
 
                                         # The following saves the contents of the registers as they will likely be
-                                        # overwritten as main is not our actual entry point, Bare_Bones is. We will
-                                        # use these 2 variables inside Bare_Bones.
+                                        # overwritten because main is not our actual entry point, Bare_Bones is. We will
+                                        # use these 2 symbols inside Bare_Bones.
 
-    movl  %eax, magic                   # Multiboot magic number
-    movl  %ebx, mbd                     # Multiboot data structure
+    movl  %eax, magic                   # EAX indicates to the OS that it was loaded by a Multiboot-compliant boot loader
+    movl  %ebx, mbd                     # EBX must contain the physical address of the Multiboot information structure
 
-    call  main                          # Call main created by gnatbind
+    call  main                          # Call main (created by gnatbind)
 
-    cli                                 # In a real kernel, execution should never get to this point.
-
+    cli                                 # Disable interrupts. then intentionally hang the system.
+                                        # CLI only affects the interrupt flag for the processor on which it is executed.
 
 hang:
-    hlt                                 # Halt machine should kernel return
+    hlt                                 # Because the HLT instruction halts until an interrupt occurs, the combination of a
+                                        # CLI followed by a HLT is used to intentionally hang the computer if the kernel returns.
+                                        # HLT is in a loop just in case a single HLT instruction fails to execute for some reason,
+                                        # (such as in the case of an NMI).
     jmp   hang
